0e14a473ab07facb5e6b75ce6d7e0f222771305b
Jarkko Oranen <jarkko.oranen@cybercom.com>
2017-01-12 18:23:30 +0200

    Simplify db_ismaster
    This method gets rid of some ugly regexp hackery and does not fail
    with MongoDB 3.4

--- lib/puppet/provider/mongodb.rb
+++ lib/puppet/provider/mongodb.rb
@@ -130,14 +130,12 @@ class Puppet::Provider::Mongodb < Puppet::Provider
   end
 
   def self.db_ismaster
-    cmd_ismaster = 'printjson(db.isMaster())'
+    cmd_ismaster = 'printjson({ismaster: db.isMaster().ismaster})'
     if mongorc_file
       cmd_ismaster = mongorc_file + cmd_ismaster
     end
     db = 'admin'
     out = mongo_cmd(db, get_conn_string, cmd_ismaster)
-    out.gsub!(/ObjectId\(([^)]*)\)/, '\1')
-    out.gsub!(/ISODate\((.+?)\)/, '\1 ')
     out.gsub!(/^Error\:.+/, '')
     res = JSON.parse out
 
